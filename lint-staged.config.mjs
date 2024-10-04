bee89c24ceb7b20707b586c7fc9e5cde851cdf96
Roman-Octavian <work.octavian@gmail.com>
2024-10-04 09:00:38 +0200

    fix lint-staged type-checking (might have to improve this later)

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -2,5 +2,5 @@ export default {
   '**/*': 'prettier --write --ignore-unknown',
   'client/*.ts?(x)': () => 'npm run types -w client',
   'server/*.ts?(x)': () => 'npm run types -w server',
-  'packages/**/*.ts?(x)': () => 'npm run types',
+  'packages/types/*.ts?(x)': () => 'npm run types -w packages/types',
 };
