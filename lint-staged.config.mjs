5e07514cde171af28bd156a13fa73d37a67a3934
Roman-Octavian <work.octavian@gmail.com>
2024-10-04 04:53:32 +0200

    add lint-staged type-checking to packages

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -2,4 +2,5 @@ export default {
   '**/*': 'prettier --write --ignore-unknown',
   'client/*.ts?(x)': () => 'npm run types -w client',
   'server/*.ts?(x)': () => 'npm run types -w server',
+  'packages/**/*.ts?(x)': () => 'npm run types',
 };
